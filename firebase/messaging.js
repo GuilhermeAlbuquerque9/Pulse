0cc3aeac254d88d5570fece27370bd1350bfbf2e
GuilhermeAlbuquerque9 <guilherme.albq10@gmail.com>
2026-05-29 17:36:19 -0300

    Update messaging.js

--- firebase/messaging.js
+++ firebase/messaging.js
@@ -1,31 +1,55 @@
 import { app } from "./firebase-config.js";
 
 import {
-  getMessaging,
-  getToken
+    getMessaging,
+    getToken
 } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-messaging.js";
 
-const messaging = getMessaging(app);
+const messaging =
+getMessaging(app);
 
-export async function registerFCM() {
+export async function registerFCM(){
 
-  try {
+    try{
 
-    const token = await getToken(
-      messaging,
-      {
-        vapidKey: "BIKU2Wu0xdPmXc3BOUTliDYYmZtNo9HRKggA5vLgVHDn7WEc8ljC4QEu6jKXu0XfBzDNGAfQkNWoorpyOA9gerY"
-      }
-    );
+        const token =
+        await getToken(
+            messaging,
+            {
+                vapidKey:
+                "BIKU2Wu0xdPmXc3BOUTliDYYmZtNo9HRKggA5vLgVHDn7WEc8ljC4QEu6jKXu0XfBzDNGAfQkNWoorpyOA9gerY"
+            }
+        );
 
-    console.log("FCM Token:", token);
+        if(token){
 
-    return token;
+            console.log(
+                "✅ FCM Token:"
+            );
 
-  } catch(error) {
+            console.log(token);
 
-    console.error(error);
+            localStorage.setItem(
+                "pulseFCMToken",
+                token
+            );
 
-  }
+            return token;
+
+        }
+
+        console.warn(
+            "Nenhum token gerado."
+        );
+
+    }
+    catch(error){
+
+        console.error(
+            "Erro FCM:",
+            error
+        );
+
+    }
 
 }
